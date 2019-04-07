e6f5aa0949e86599702c13ecf73a09b31b37ec98
Faiz Akram <faiz.akram@compunnel.in>
2019-04-07 13:39:21 +0530

    commit

--- src/main/java/com/example/easynotes/utils/SendMailUtils.java
+++ src/main/java/com/example/easynotes/utils/SendMailUtils.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,29 +14,31 @@ import com.example.easynotes.repository.CopyObjectRepository;
 
 @Component
 public class SendMailUtils {
-	@Autowired
-	private EmailNotificationImpl emailNotificationImpl;
+	/*@Autowired
+	private EmailNotificationImpl emailNotificationImpl;*/
 	@Autowired
 	private CopyObjectRepository copyObjectRepository;
-	
+
 	@Autowired
 	private S3Utility s3Utility;
 
-	@Scheduled(cron = "0 * * ? * *")
+	/*@Scheduled(cron = "0 * * ? * *")
 	public void runScheduler() {
-		/*
-		 * Map<String, Object> emailMap
-		 * =emailNotificationImpl.insertEmailRecordForRest("faiz.krm@gmail.com",
-		 * "Faiz Akram"); emailNotificationImpl.sendEmailNotification(emailMap,
-		 * "faiz.krm@gmail.com", true, null, null);
-		 */
+		
+		  Map<String, Object> emailMap
+		  =emailNotificationImpl.insertEmailRecordForRest("faiz.krm@gmail.com",
+		  "Faiz Akram"); emailNotificationImpl.sendEmailNotification(emailMap,
+		  "faiz.krm@gmail.com", true, null, null);
 		System.out.println("Working");
-	}
-	
+	}*/
+
 	@Scheduled(cron = "0 * * ? * *")
 	public void removeFileOnS3() {
 		List<CopyObjects> copyObjects = copyObjectRepository.findByCurrentTime(new Date());
-		s3Utility.deleteObjectsFromPublic(copyObjects.stream().map(CopyObjects::getFileName).collect(Collectors.toList()));
-		copyObjectRepository.deleteAll(copyObjects);
+		if (CollectionUtils.isNotEmpty(copyObjects)) {
+			s3Utility.deleteObjectsFromPublic(
+					copyObjects.stream().map(CopyObjects::getFileName).collect(Collectors.toList()));
+			copyObjectRepository.deleteAll(copyObjects);
+		}
 	}
 }
