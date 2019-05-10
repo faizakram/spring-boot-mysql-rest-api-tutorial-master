a9039f1ee3f63217d530eeafda5ba6626cf3c70f
Faiz Akram <faiz.akram@compunnel.in>
2019-05-10 18:15:00 +0530

    commited

---
+++ src/main/java/com/example/easynotes/controller/ReqestBodyExample.java
@@ -0,0 +1,27 @@
+package com.example.easynotes.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.easynotes.utils.ContactInfo;
+
+@RestController
+public class ReqestBodyExample {
+
+	
+
+	@PostMapping("uploads")
+	public Map<String, Object> requestBody(@RequestParam ContactInfo contactInfo) throws IOException {
+		Map<String, Object> maps = new HashMap<>();
+		maps.put("contactInfo", contactInfo);
+		return maps;
+	}
+
+	
+
+}
---
+++ src/main/java/com/example/easynotes/utils/ContactInfo.java
@@ -0,0 +1,45 @@
+package com.example.easynotes.utils;
+
+public class ContactInfo {
+
+	private Integer id;
+	private String name;
+	private Integer age;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public Integer getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+}
